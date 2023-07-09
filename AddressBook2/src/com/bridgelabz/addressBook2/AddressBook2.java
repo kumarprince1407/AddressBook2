62dd34389f21bb7c3ead7cbac479282dcb7717a7
Prince Kumar <kumarprince1407@gmail.com>
2023-07-09 16:19:47 +0530

    Added initial commit

---
+++ AddressBook2/src/com/bridgelabz/addressBook2/AddressBook2.java
@@ -0,0 +1,7 @@
+package com.bridgelabz.addressBook2;
+/**/
+public class AddressBook2 {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Address Book program.");
+    }
+}
