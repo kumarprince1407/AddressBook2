ad7c2545a1968bcf7878ce8fe4f5a483f49dc519
Prince Kumar <kumarprince1407@gmail.com>
2023-07-09 17:38:02 +0530

    Added Use Case 1

--- AddressBook2/src/com/bridgelabz/addressBook2/AddressBook2.java
+++ AddressBook2/src/com/bridgelabz/addressBook2/AddressBook2.java
@@ -1,7 +1,110 @@
 package com.bridgelabz.addressBook2;
-/**/
+//UC1
+/*Ability to create a Contacts in Address
+Book with first and last names, address,
+city, state, zip, phone number and
+email...*/
+
+class Contact{
+    private String firstName;
+    private String lastName;
+    private String address;
+    private String city;
+    private String state;
+    private String zip;
+    private long phone;
+    private String email;
+
+    public Contact(String firstName, String lastName, String address, String city, String state, String zip, long phone, String email){
+        this.firstName=firstName;
+        this.lastName=lastName;
+        this.address=address;
+        this.city=city;
+        this.state=state;
+        this.zip=zip;
+        this.phone=phone;
+        this.email=email;
+    }
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+    public String getZip(){
+        return zip;
+    }
+    public void setZip(String zip){
+        this.zip=zip;
+    }
+
+    public long getPhone() {
+        return phone;
+    }
+
+    public void setPhone(long phone) {
+        this.phone = phone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public String toString(){
+    return "Contact:"+
+            " First name: "+ firstName+
+            " Last name: "+ lastName +
+            " Address: " + address +
+            " City: " + city +
+            " State: "+ state +
+            " Phone: " + phone +
+            " Email: " + email;
+    }
+}
 public class AddressBook2 {
     public static void main(String[] args) {
         System.out.println("Welcome to Address Book program.");
+        Contact contact = new Contact("John", "Wick", "Beverly Hills","San Francisco","California","355678",789943456,"johnwick@gmail.com");
+        Contact contact2 = new Contact("Tom", "Hanks", "Beverly Hills","San Francisco","California","367890",907943456,"johnwick@gmail.com");
+        System.out.println(contact.toString());
+        System.out.println(contact2.toString());
     }
 }
